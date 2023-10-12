11868dacfd5cd4fc42872a7ac0fd0bbc70802c35
TA\adarsh.j <adarsh.j@travancoreanalytics.com>
2023-10-12 15:08:59 +0530

    Create signup api

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -11,7 +12,9 @@ mongoose
   .catch((err) => console.log(err));
 
 const app = express();
+app.use(express.json());
 app.listen(3000, () => {
   console.log("Server is running on port 3000!!!!");
 });
 app.use("/api/user/", userRouter);
+app.use("/api/auth/", authRouter);
